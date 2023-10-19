8590684586fc9ed5de99b0a3168d34a654b0d6ae
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-10-19 17:31:34 +0530

    Completed Intigration for Most Popular

--- components/Navbar.js
+++ components/Navbar.js
@@ -5,7 +5,7 @@ function Navbar() {
   const options = (size) => Array.from({ length: size }, (_, i) => `${i + 1} option`);
 
   return (
-    <div className="bg-gray-800 h-[80px] z-10 top-0 fixed left-0 right-0 flex items-center justify-center text-white">
+    <div className="bg-gray-800 h-[80px] z-[1100] shadow[10px_0px_10px_gray] top-0 fixed left-0 right-0 flex items-center justify-center text-white">
       <Select title="Type" options={options(50)} />
       <Select title="Type" options={options(10)} />
       <Select title="Type" options={options(15)} />
