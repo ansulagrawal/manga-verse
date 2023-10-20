e41d05fa91a01c6f25615d75d25394ab54f51532
Ansul Agrawal <ansulagrawal9@gmail.com>
2023-10-20 16:06:00 +0530

    Fix the title and added default color

--- components/Card.js
+++ components/Card.js
@@ -7,6 +7,7 @@ import relativeTime from 'dayjs/plugin/relativeTime';
 dayjs.extend(relativeTime);
 
 const genereColor = {
+  default: 'bg-gray-400 text-gray-700',
   romance: 'bg-red-200 text-red-700',
   comedy: 'bg-orange-200 text-orange-700',
   'slice of life': 'bg-green-200 text-green-700',
@@ -29,36 +30,36 @@ const genereColor = {
 function Card({ title, desc, author, genere, time, volume, chapter, height = '200px', id, imageUrl }) {
   return (
     <Link href={`manga/${id}`}>
-    <div className={`relative flex bg-gray-800 h-full text-white p-3 mx-3 rounded-xl`}>
-      <div className="aspect-[7/8] object-cover relative h-full">
-        <Image className="pointer-events-none select-none object-cover" src={`${process.env.IMAGE_URL}/covers/${id}/${imageUrl}.256.jpg`} alt="Cover Image" fill loading="lazy" />
-      </div>
-
-      <div className="pl-5">
-        <b className="text-xl break-all line-clamp-1">{title}</b>
-
-        <p className="text-sm text-gray-200 mb-2">Author: {author}</p>
-
-        {(volume || chapter) && (
-          <div className="flex gap-2 mb-2">
-            {volume && <p className="text-sm text-gray-200">Vol:{volume}</p>}
-            {chapter && <p className="text-sm text-gray-200">Chapter:{chapter}</p>}
-          </div>
-        )}
-
-        <p className="text-sm text-gray-400 mb-2 line-clamp-3 break-word">{desc}</p>
-
-        <div className="flex flex-wrap gap-1 mb-5">
-          {genere?.slice(0, 4)?.map(g => (
-            <div className={`text-xs inline-flex items-center font-bold leading-sm uppercase px-3  py-1 rounded-full ${genereColor[g.toLowerCase()]}`} key={g}>
-              {g}
-            </div>
-          ))}
+      <div className={`relative flex bg-gray-800 h-full text-white p-3 mx-3 rounded-xl`}>
+        <div className="aspect-[7/8] object-cover relative h-full">
+          <Image className="pointer-events-none select-none object-cover" src={`${process.env.IMAGE_URL}/covers/${id}/${imageUrl}.256.jpg`} alt="Cover Image" fill loading="lazy" />
         </div>
 
-        {time && <div className="absolute bottom-1 text-gray-300 right-5">Updeted: {dayjs(time).fromNow()}</div>}
+        <div className="pl-5">
+          <b className="text-xl break-all line-clamp-1">{title}</b>
+
+          <p className="text-sm text-gray-200 mb-2">Author: {author}</p>
+
+          {(volume || chapter) && (
+            <div className="flex gap-2 mb-2">
+              {volume && <p className="text-sm text-gray-200">Vol:{volume}</p>}
+              {chapter && <p className="text-sm text-gray-200">Chapter:{chapter}</p>}
+            </div>
+          )}
+
+          <p className="text-sm text-gray-400 mb-2 line-clamp-3 break-word">{desc}</p>
+
+          <div className="flex flex-wrap gap-1 mb-5">
+            {genere?.slice(0, 4)?.map(g => (
+              <div className={`text-xs inline-flex items-center font-bold leading-sm uppercase px-3  py-1 rounded-full ${genereColor[g.toLowerCase()] || genereColor.default}`} key={g}>
+                {g}
+              </div>
+            ))}
+          </div>
+
+          {time && <div className="absolute bottom-1 text-gray-300 right-5">Updeted: {dayjs(time).fromNow()}</div>}
+        </div>
       </div>
-    </div>
     </Link>
   );
 }
